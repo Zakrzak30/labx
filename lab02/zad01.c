3cbcc6ed458d1a658cd5acdb289af2f95e4dc829
mzakrzewski <michzak30@gmail.com>
2016-03-03 22:37:33 +0100

    Update zad01.c

--- lab02/zad01.c
+++ lab02/zad01.c
@@ -1,29 +1,27 @@
 #include<stdio.h>
 
 int main(){
-	int liczba[8];
-	char podana;
-	int i,j,rzutowana,reszta;
+	int tab[8];
+	char tekst;
+	int i, j, obraz, reszta;
 
-	printf("Program drukuje kod znaku w zapisie binarnym oraz kod znaku nastepnej linii\n");
-
-	do{
-		scanf("%c",&podana);
-		rzutowana=(int)podana;
-		for(i=0;i<8;i++)
-			liczba[i]=0;
-			j=7;
-		while(rzutowana!=0){
+	while( tekst != EOF ){
+		scanf("%c", &tekst );
+		obraz = (int)tekst;
+		for( i=0; i<8; i++ )
+			tab[i] = 0;
+			j = 7;
+		while( obraz != 0 ){
 			
-			reszta=rzutowana%2;
-			rzutowana=rzutowana/2;
-			if(reszta==1)
-				liczba[j]=1;
+			reszta = obraz%2;
+			obraz = obraz/2;
+			if( reszta == 1 )
+				tab[j] = 1;
 			j--;
 		}
-		for(i=0;i<8;i++)
-			printf("%d",liczba[i]);
+		for(i=0; i<8; i++)
+			printf("%d", tab[i]);
 		printf("\n");
-	}while(podana!=EOF);
+	}
 return 0;
 }
