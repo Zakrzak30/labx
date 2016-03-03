10d934cbc698d311805c9938d5b7b12cb4672753
mzakrzewski <michzak30@gmail.com>
2016-03-03 22:30:51 +0100

    Update zad00.c

--- lab02/zad00.c
+++ lab02/zad00.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-  printf("A. 5&3 = %i\n", 5&3);
-  printf("B. 5|3 = %i\n", 5|3);
-  printf("C. 7<<2&7 = %i\n", 7<<2&7);
-  printf("D. 7<<(2&7) = %i\n", 7<<(2&7));
-  printf("E. ((-1)<<8)>>16 = %i\n", ((-1)<<8)>>16);
-  printf("F. 13^9 = %i\n", 13^9);
+  printf("a). 5&3 = %i\n", 5&3);
+  printf("b). 5|3 = %i\n", 5|3);
+  printf("c). 7<<2&7 = %i\n", 7<<2&7);
+  printf("d). 7<<(2&7) = %i\n", 7<<(2&7));
+  printf("e). ((-1)<<8)>>16 = %i\n", ((-1)<<8)>>16);
+  printf("f). 13^9 = %i\n", 13^9);
   return 0;
 }
