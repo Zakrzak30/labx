e12d44283363151f22a11290e13fc0d3107aa25a
mzakrzewski <michzak30@gmail.com>
2016-03-18 08:08:05 +0100

    Update zad01.c

--- lab03/zad01.c
+++ lab03/zad01.c
@@ -8,7 +8,7 @@ int main()
 printf("|typ        |       wartosc min   |          wartosc max|       ziarno| precyzja|we/wy|\n");
 printf("|-----------|---------------------|---------------------|-------------|---------|-----|\n");
 printf("|short      | %20i| %20i|             |         | i   |\n",SHRT_MIN,SHRT_MAX);	
-printf("|int        | %20d| %20d|             |         | d   |\n",SHRT_MIN,SHRT_MAX);	
+printf("|int        | %20d| %20d|             |         | d   |\n",INT_MIN,INT_MAX);	
 printf("|long       | %20li| %20li|             |         | li  |\n",LONG_MIN,LONG_MAX);
 printf("|long long  | %20lli| %20lli|             |         | lli |\n",LLONG_MIN,LLONG_MAX);
 printf("|float      | %20e| %20e| %e|        %i| lli |\n",FLT_MIN,FLT_MAX,FLT_EPSILON,FLT_DIG);
