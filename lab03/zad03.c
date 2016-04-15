ac878146d89627ff5fbb9c5574f1cf7d3d7fb6e6
mzakrzewski <michzak30@gmail.com>
2016-04-15 10:53:57 +0200

    Update zad03.c

--- lab03/zad03.c
+++ lab03/zad03.c
@@ -1,18 +1,30 @@
-//wyjasnic dwa ostatnie
+//Sprawdzić, wykonując odpowiednie testy, jak działają operacje rzutowania z typów ,,mniejszych'' do ,,większych'' i odwrotnie. Na przykład, czemu są równe:
+//(int)FLT_MAX
+//(int)DBL_MAX
+//(double)LDBL_MAX
 
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
-
+#define printf __mingw_printf // gdy pracujemy na windowsie
 int main()
 {
-  printf(" Rzutowanie FLT_MAX do (int)FLT_MAX: '%e' do '%i' \n" , FLT_MAX , (int)FLT_MAX );
-  printf(" Rzutowanie DBL_MAX do (int)DBL_MAX: '%e' do '%i' \n" , DBL_MAX , (int)DBL_MAX );
-  printf(" Rzutowanie LDBL_MAX do (double)LDBL_MAX: '%Le' do '%e' \n" , LDBL_MAX , (double)LDBL_MAX );
-  printf(" Rzutowanie INT_MAX do (float)INT_MAX: '%i' do '%e' \n" , INT_MAX  , (float)INT_MAX );
-  printf(" Rzutowanie LLONG_MAX do (double)LLONG_MAX: '%lli' do '%le' \n" , LLONG_MAX , (double)LLONG_MAX );
-
+  printf("FLT_MAX -> (int)FLT_MAX:\n%e -> %i\n\n", FLT_MAX, (int)FLT_MAX);
+  //wyznacza maksymalna (czyli jak najblizsza zadanej(float)) liczbe w innym typie (int)
+  printf("INT_MAX -> (float)INT_MAX:\n%i -> %e\n\n", INT_MAX, (float)INT_MAX);
+  //wypisuje maksymalna wartosc inta (float > int)
+  printf("DBL_MAX -> (int)DBL_MAX:\n%e -> %i\n", DBL_MAX, (int)DBL_MAX);
+  //wyznacza maksymalna (czyli jak najblizsza zadanej(double)) liczbe w innym typie (int)
+  printf("Wartosc double max wychodzi poza maksymalna wartosc inta, zatem drukowana jest wartosc maksymalna inta.\n\n");
+  printf("INT_MAX -> (double)INT_MAX:\n%i -> %e\n\n", INT_MAX, (double)INT_MAX);
+  //wypisuje maksymalna wartosc inta (double > int)
+  printf("LDBL_MAX -> (double)LDBL_MAX:\n%Le -> %e\n\n", LDBL_MAX, (double)LDBL_MAX);
+  printf("(double)LDBL_MAX podzielone przez 2:\n %e \n\n", (double)LDBL_MAX/2);
+  printf("(double)LDBL_MAX podzielone przez 4:\n %e \n\n", (double)LDBL_MAX/4);
+  //wypisuje infinity
+  printf("LONG_MAX -> (float)LONG_MAX:\n%li -> %e\n\n", LONG_MAX, (float)LONG_MAX);
+  //wypisuje maksymalna wartosc longa(float > long)
+  printf("INT_MAX -> (float)INT_MAX:\n%i -> %e\n\n", INT_MAX, (float)INT_MAX);
+  //wypisuje maksymalna wartosc inta (float > int)
   return 0;
 }
-  // rzutowanie z mniejszego typu na wiekszy różnica w wyswietlanych wynikach jest spowodowana
-  // tym że w postaci ae+b liczby na a różnią się o mniej niż ziarno
